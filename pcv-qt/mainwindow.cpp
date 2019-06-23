309b73cc2212e356b99f6e9bd6402a9638407a39
girafov <mpolotskaia64@gmai.com>
2019-06-23 00:40:24 -0700

    add qt folder

---
+++ pcv-qt/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QFileDialog"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QFileDialog::getOpenFileNames(this, tr("Open File"),"/path/to/file/",tr("Las Files (*.las)"));
+}
